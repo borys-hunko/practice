18d844fbd5c855c8f2cf4571b6ea88108fe2c320
Borys Hunko <borys.hunko11@gmail.com>
2021-04-08 13:01:06 +0300

    add Beverage class

---
+++ src/main/java/task1/entities/Beverage.java
@@ -0,0 +1,73 @@
+package task1.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Beverage extends Product {
+    private LocalDate expirationDate;
+    private double nutritionalValue;
+    private double volume;
+
+    public Beverage() {
+    }
+
+    public Beverage(String name,
+                    BigDecimal price,
+                    LocalDate manufactureDate,
+                    LocalDate expirationDate,
+                    double nutritionalValue,
+                    double volume) {
+        super(name, price, manufactureDate);
+        this.expirationDate = expirationDate;
+        this.nutritionalValue = nutritionalValue;
+        this.volume = volume;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public double getNutritionalValue() {
+        return nutritionalValue;
+    }
+
+    public void setNutritionalValue(double nutritionalValue) {
+        this.nutritionalValue = nutritionalValue;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Beverage{" +
+                "expirationDate=" + expirationDate +
+                ", nutritionalValue=" + nutritionalValue +
+                ", volume=" + volume +
+                "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Beverage beverage = (Beverage) o;
+        return Double.compare(beverage.nutritionalValue, nutritionalValue) == 0 && Double.compare(beverage.volume, volume) == 0 && expirationDate.equals(beverage.expirationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), expirationDate, nutritionalValue, volume);
+    }
+}
