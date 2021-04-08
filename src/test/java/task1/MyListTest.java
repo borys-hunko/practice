548ead91030197151542bf17dbb8fe570a7c79ea
Borys Hunko <borys.hunko11@gmail.com>
2021-04-08 14:29:16 +0300

    Added method in MyListTest which test if capacity of list increased

--- src/test/java/task1/MyListTest.java
+++ src/test/java/task1/MyListTest.java
@@ -47,4 +47,13 @@ public class MyListTest {
         products.add(null);
         assertEquals(1,products.size());
     }
+
+    @Test
+    @DisplayName("test whether add method increases capacity of array in the list")
+    void testMemoryAllocationInAdd(){
+        for (int i = 0; i < 11; i++) {
+            products.add(null);
+        }
+        assertEquals(11,products.size());
+    }
 }
