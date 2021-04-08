4dceed996422f33fa5d5db9f3d4765eab8e04970
Borys Hunko <borys.hunko11@gmail.com>
2021-04-08 16:21:48 +0300

    added method in MyListTest to test set method in MyList after incorrect index passed

--- src/test/java/task1/MyListTest.java
+++ src/test/java/task1/MyListTest.java
@@ -120,4 +120,11 @@ public class MyListTest {
         products.set(0, newValue);
         assertEquals(newValue, products.get(0));
     }
+
+    @Test
+    void testPassingIncorrectIndexToSet() {
+        Throwable exception = assertThrows(IndexOutOfBoundsException.class,
+                () -> products.set(1000, null));
+        assertEquals("no element with index 1000", exception.getMessage());
+    }
 }
