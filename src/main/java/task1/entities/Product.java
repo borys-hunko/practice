44f6c6b0c6bfd57e571ac11f556ae49e914494b5
Borys Hunko <borys.hunko11@gmail.com>
2021-04-12 13:22:28 +0300

    add equals and hashCode in Product

--- src/main/java/task1/entities/Product.java
+++ src/main/java/task1/entities/Product.java
@@ -53,6 +53,19 @@ public class Product {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(name, product.name) && Objects.equals(price, product.price) && Objects.equals(manufactureDate, product.manufactureDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, manufactureDate);
+    }
+
+    @Override
     public String toString() {
         return "Product{" +
                 "name='" + name + '\'' +
